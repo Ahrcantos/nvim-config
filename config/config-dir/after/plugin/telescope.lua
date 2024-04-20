1b6e99ce20b16e061592ccc719b14e1fe8816c61
Ahrcantos <ahrcantos99@gmail.com>
2024-04-20 14:42:56 +0200

    Add diagnostics finder

--- config/config-dir/after/plugin/telescope.lua
+++ config/config-dir/after/plugin/telescope.lua
@@ -3,3 +3,5 @@ vim.keymap.set('n', '<leader>v', builtin.find_files, {})
 -- vim.keymap.set('n', '<leader>v', builtin.git_files, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>s', builtin.live_grep)
+
+vim.keymap.set('n', '<leader>ld', builtin.diagnostics, {})
